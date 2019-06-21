d465037cff38f6b27d2d7eebbd77a947348ee1e1
xusumu <xusumu@gmail.com>
2019-06-21 17:44:43 +0800

    flaten

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let v = [];
+  v = [].concat.apply([], collection);
+  return v;
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,10 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let v = [];
+  v = [].concat.apply([], collection);
+  return v.filter(function (element, index, self) { return self.indexOf(element) === index; });
+  return v;
 }
 
 module.exports = double_to_one;
