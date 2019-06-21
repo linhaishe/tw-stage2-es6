b65b41ad6571ae6899d62c3fdffbba9d48e3627e
xusumu <xusumu@gmail.com>
2019-06-21 17:32:01 +0800

    update

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  let result = collection.filter(function(x){
+    return x%2 === 0;
+  })
+  console.log(result);
+  return result;
 }
 
 module.exports = choose_even;
