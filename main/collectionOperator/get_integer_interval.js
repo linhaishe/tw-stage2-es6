3b63ad2e09ad5b7b02b9e4478403aff4ad5f2a88
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-06-22 14:13:55 +0800

    2/day

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,23 +1,37 @@
-'use strict';
+"use strict";
 
-function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
-  let n = [];
-  if(number_a < number_b){
-    for(let i=number_a;i<=number_b;i++){
-      n.push(i);
+// function get_integer_interval(number_a, number_b) {
+//   //在这里写入代码
+//   let n = [];
+//   if (number_a < number_b) {
+//     for (let i = number_a; i <= number_b; i++) {
+//       n.push(i);
+//     }
+//   } else if (number_a > number_b) {
+//     for (let i = number_a; i >= number_b; i--) {
+//       n.push(i);
+//     }
+//   } else if ((number_a = number_b)) {
+//     n.push(number_a);
+//   }
+//   return n;
+// }
+
+let get_integer_interval = (num1, num2) => get_integer_interval_arr(num1, num2);
+
+let get_integer_interval_arr = (num1, num2) => {
+  let integerArr = [];
+  if (num2 > num1) {
+    for (let i = num1; i <= num2; i++) {
+      integerArr.push(i);
+    }
+  } else {
+    //num2<num1,num1>num2
+    for (let i = num1; i >= num2; i--) {
+      integerArr.push(i);
     }
   }
-  else if(number_a > number_b){
-    for(let i=number_a;i>=number_b;i--){
-      n.push(i);
-    }
-  }
-  else if(number_a = number_b){
-    n.push(number_a)
-  }
-  return n;
-}
+  return integerArr;
+};
 
 module.exports = get_integer_interval;
-
--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -1,17 +1,31 @@
-'use strict';
+"use strict";
 
-function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
-  let result = [];
-  for (let j = 0; j < collection_b.length; j++) {
-    for (let i = 0; i < collection_a.length; i++) {
-      if (collection_a[i] === collection_b[j]) {
-        result.push(collection_b[j]);
+// function get_intersection(collection_a, collection_b) {
+//   //在这里写入代码
+//   let result = [];
+//   for (let j = 0; j < collection_b.length; j++) {
+//     for (let i = 0; i < collection_a.length; i++) {
+//       if (collection_a[i] === collection_b[j]) {
+//         result.push(collection_b[j]);
+//       }
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+
+let get_intersection = (arr1, arr2) => getArr(arr1, arr2);
+
+let getArr = (arr1, arr2) => {
+  let res = [];
+  for (let j = 0; j < arr1.length; j++) {
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1[i] === arr2[j]) {
+        res.push(arr2[j]);
       }
     }
   }
-  console.log(result);
-  return result;
-}
+  return res;
+};
 
 module.exports = get_intersection;
